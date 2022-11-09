c62538192ad46f58a3e45713c3515a409dfc1ecd
Pekka Kaitaniemi <pekka.kaitaniemi@tocoman.com>
2022-11-09 03:50:52 +0200

    Switch to Proxy instead of SProxy

--- src/Shoronpo.purs
+++ src/Shoronpo.purs
@@ -4,16 +4,16 @@ import Prim.Row as Row
 import Prim.RowList as RL
 import Prim.Symbol as Symbol
 import Record.Format as RF
-import Type.Prelude (Proxy, SProxy(..))
+import Type.Prelude (Proxy(..))
 
 formatSymbol
   :: forall string flist row out proxyOrRec
    . RF.Parse string flist
   => FormatSymbolParsed flist row "" out
-  => SProxy string
+  => Proxy string
   -> proxyOrRec row
-  -> SProxy out
-formatSymbol _ _ = SProxy
+  -> Proxy out
+formatSymbol _ _ = Proxy
 
 class FormatSymbolParsed (flist :: RF.FList) (row :: Row Type) (acc :: Symbol) (out :: Symbol)
   | flist -> row acc out
@@ -22,7 +22,7 @@ instance nilFormatSymbolParsed :: FormatSymbolParsed RF.FNil row out out
 
 instance consVarFormatSymbolParsed ::
   ( Symbol.Append acc sym acc'
-  , Row.Cons var (SProxy sym) row' row
+  , Row.Cons var (Proxy sym) row' row
   , FormatSymbolParsed tail row acc' out
   ) => FormatSymbolParsed (RF.FCons (RF.Var var) tail) row acc out
 
@@ -35,17 +35,17 @@ intercalateRowLabels
   :: forall row x out proxyOrRecord
    . IntercalateRowLabels row x out
   => proxyOrRecord row
-  -> SProxy x
-  -> SProxy out
-intercalateRowLabels _ _ = SProxy
+  -> Proxy x
+  -> Proxy out
+intercalateRowLabels _ _ = Proxy
 
 intercalateRecordLabels
   :: forall row x out
    . IntercalateRowLabels row x out
   => Proxy { | row }
-  -> SProxy x
-  -> SProxy out
-intercalateRecordLabels _ _ = SProxy
+  -> Proxy x
+  -> Proxy out
+intercalateRecordLabels _ _ = Proxy
 
 class IntercalateRowLabels (row :: Row Type) (x :: Symbol) (out :: Symbol)
 
@@ -73,17 +73,17 @@ intercalateRowValues
   :: forall row x out proxyOrRecord
    . IntercalateRowValues row x out
   => proxyOrRecord row
-  -> SProxy x
-  -> SProxy out
-intercalateRowValues _ _ = SProxy
+  -> Proxy x
+  -> Proxy out
+intercalateRowValues _ _ = Proxy
 
 intercalateRecordValues
   :: forall row x out
    . IntercalateRowValues row x out
   => Proxy { | row }
-  -> SProxy x
-  -> SProxy out
-intercalateRecordValues _ _ = SProxy
+  -> Proxy x
+  -> Proxy out
+intercalateRecordValues _ _ = Proxy
 
 class IntercalateRowValues (row :: Row Type) (x :: Symbol) (out :: Symbol)
 
@@ -99,10 +99,10 @@ instance nilIntercalateRowValuesImpl :: IntercalateRowValuesImpl RL.Nil x out ou
 
 instance consNilIntercalateRowValuesImpl ::
   ( Symbol.Append acc value acc'
-  ) => IntercalateRowValuesImpl (RL.Cons name (SProxy value) RL.Nil) x acc acc'
+  ) => IntercalateRowValuesImpl (RL.Cons name (Proxy value) RL.Nil) x acc acc'
 
 else instance consIntercalateRowValuesImpl ::
   ( Symbol.Append value x s
   , Symbol.Append acc s acc'
   , IntercalateRowValuesImpl tail x acc' out
-  ) => IntercalateRowValuesImpl (RL.Cons name (SProxy value) tail) x acc out
+  ) => IntercalateRowValuesImpl (RL.Cons name (Proxy value) tail) x acc out
